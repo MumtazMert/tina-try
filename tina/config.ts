5387ef2a647b18b9a56c00ab02b1862fdd99e78e
Mumtaz Mert Tekdemir <mumtazmert@gmail.com>
2025-08-13 16:48:58 +0300

    try-6

--- tina/config.ts
+++ tina/config.ts
@@ -63,12 +63,6 @@ export default defineConfig({
                   list: true,
                },
                {
-                  type: 'string',
-                  name: 'author',
-                  label: 'Author',
-                  description: 'The author of the post',
-               },
-               {
                   type: 'object',
                   name: 'blocks',
                   label: 'Content Blocks',
